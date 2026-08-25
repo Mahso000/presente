e96a846bf387b7d5b8643d438824f67e62df9b52
Mahso000 <mariasophiaalves2906@gmail.com>
2026-08-25 00:38:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+
+  setTimeout(() => {
+    document.querySelector(".next-button").classList.add("show");
+  }, 6000);
+};
