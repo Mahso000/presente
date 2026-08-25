5eb43f2ab119f6798b904ea7948d01412dd975a8
Mahso000 <mariasophiaalves2906@gmail.com>
2026-08-25 00:50:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 window.addEventListener("load", () => {
+
   setTimeout(() => {
     document.body.classList.remove("not-loaded");
   }, 1000);
 
   setTimeout(() => {
-    const arrow = document.querySelector(".next-button");
+    const arrow = document.getElementById("next-button");
 
     if (arrow) {
       arrow.classList.add("show");
     }
   }, 6000);
+
 });
