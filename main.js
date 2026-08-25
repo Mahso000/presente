07574dd56473aee62b00f72fcdbf110ac9995ad0
Mahso000 <mariasophiaalves2906@gmail.com>
2026-08-25 01:22:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 window.addEventListener("load", () => {
 
+  const arrow = document.getElementById("next-button");
+
+  // Esconde imediatamente
+  if (arrow) {
+    arrow.style.display = "none";
+  }
+
+  // Animações das flores
   setTimeout(() => {
     document.body.classList.remove("not-loaded");
   }, 1000);
 
+  // Mostra a seta depois de 6 segundos
   setTimeout(() => {
-    const arrow = document.getElementById("next-button");
-
     if (arrow) {
       arrow.style.display = "block";
 
